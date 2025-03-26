41734f2fa3c80d4047e60adc7ddd7e2f62aeac2d
correa-tech <guilhermeluis899@gmail.com>
2025-03-25 21:58:29 -0300

    first commit

---
+++ nunbers.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const ebolicaoF = 373.0 
+
+func main() {
+
+	var temK float64 = ebolicaoF
+
+	var tempC float64 = temK - 273
+
+	fmt.Println("A temperatura em K: ", + temK)
+	fmt.Println("A temperatura em c: ", + tempC)
+
+
+}
